9a0a7bf3a8c26f52cf1fac6c558ed67dfc483174
trandinhquy97 <trandinhquy97@gmail.com>
2020-05-07 15:44:42 +0700

    Updates

---
+++ precache-manifest.3d596f0640f2bd07a20fc3253d6d9bdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2806246d6aced5da5db56867ef37256",
+    "url": "/newwordreact/index.html"
+  },
+  {
+    "revision": "73c75ec68af22ddc0784",
+    "url": "/newwordreact/static/css/main.3587d2ee.chunk.css"
+  },
+  {
+    "revision": "4fa4dd31ea9fd46dbc61",
+    "url": "/newwordreact/static/js/2.f5a9f850.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/newwordreact/static/js/2.f5a9f850.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73c75ec68af22ddc0784",
+    "url": "/newwordreact/static/js/main.d5bfdda7.chunk.js"
+  },
+  {
+    "revision": "98237105470bc6900741",
+    "url": "/newwordreact/static/js/runtime-main.45c7df5b.js"
+  }
+]);
